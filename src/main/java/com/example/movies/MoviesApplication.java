75efad39d039b6e8d5a8aa2cda85646c5a33a6ef
Rahul <rahulreddy3166@gmail.com>
2026-04-30 09:22:43 -0400

    Refactor: Moved WebClient bean to MovieAppConfig as per comment and also used ResponseEntity

--- src/main/java/com/example/movies/MoviesApplication.java
+++ src/main/java/com/example/movies/MoviesApplication.java
@@ -2,20 +2,12 @@ package com.example.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
-	}
-
-	// WebClient setup
-	@Bean
-	public WebClient.Builder webClientBuilder() {
-		return WebClient.builder();
 	}
 
 }
--- src/main/java/com/example/movies/controller/MovieController.java
+++ src/main/java/com/example/movies/controller/MovieController.java
@@ -3,6 +3,8 @@ package com.example.movies.controller;
 import com.example.movies.dto.Movie;
 import com.example.movies.dto.MovieResponse;
 import com.example.movies.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,15 +22,24 @@ public class MovieController {
     }
 
     @GetMapping(value = "/movies")
-    public List<Movie> getAllMovies() {
-        return movieService.getAllMoviesMultithreaded();
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieService.getAllMoviesMultithreaded();
+        if (movies == null || movies.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(movies);
     }
 
     @GetMapping(value = "/movies", params = {"title"})
-    public MovieResponse getMoviesByQuery(
+    public ResponseEntity<MovieResponse> getMoviesByQuery(
             @RequestParam("title") String title,
             @RequestParam(value = "year", required = false) Integer year,
             @RequestParam(value = "page", required = false) Integer page) {
-        return movieService.getMoviesWithParams(title, year, page);
+        MovieResponse response =  movieService.getMoviesWithParams(title, year, page);
+
+        if (response == null || response.getData() == null || response.getData().isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(response);
     }
 }
